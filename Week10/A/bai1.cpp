bac48f67f7b984186a7c1d24f841b3eb1428c2b4
gwncmh <161003754+gwncmh@users.noreply.github.com>
2024-04-26 02:04:45 +0700

    Create bai1.cpp

---
+++ Week10/A/bai1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+char* concat(const char* a, const char* b)
+{
+    int len1=strlen(a);
+    int len2=strlen(b);
+    char* re = new char[len1+len2+1];
+    strcpy(re, a);
+    strcat(re, b);
+    return re;
+}
+
+int main()
+{
+    const char* a = "Hello ";
+    const char* b = "World";
+    char* s = concat(a, b);
+    cout<<s<<endl;
+    delete[] s;
+}
